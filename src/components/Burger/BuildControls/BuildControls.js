66938872ed031732f6b7d0a4247080b9728cd749
Sergio Morante <smorantecano@gmail.com>
2018-06-09 18:53:24 +0200

    [#001] Build Control, add an emoji icon to Current price title

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,7 +12,12 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        <p>
+            Current Price 
+            {/*  Emoji*/} 
+            <span role="img" aria-label="Dolars">💰</span>: 
+            <strong>{props.price.toFixed(2)}</strong>
+        </p>
         {controls.map(ctrl => (
             <BuildControl
                 key={ctrl.label}
